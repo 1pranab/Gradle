15efc3d8165d19d74ca574edcfd0012f29c83bf9
Pranab Ranjan <pranabranjan.1999@gmail.com>
2025-02-18 20:47:04 +0530

    Second commit

--- src/main/java/com/pr/controller/EmployeeController.java
+++ src/main/java/com/pr/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.pr.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class EmployeeController {
 
         Integer i = id;
         return new ResponseEntity<String>("The employee id is "+i, HttpStatus.OK);
+    }
+
+    @PostMapping("/create/{id}")
+    public ResponseEntity<String> showEmp(Integer id){
+        return new ResponseEntity<String>("Employee created",HttpStatus.CREATED);
     }
 }
